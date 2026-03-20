a0aea884087ce93a95b5910bfadb39a84cdc633a
felipeluisfs <luisfelipe.fs@outlook.com>
2026-03-20 17:57:44 -0300

    Incluir botão Notify Me

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../products';
 
 @Component({
   selector: 'app-product-alerts',
+  standalone: true,
+  imports: [],
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  styleUrl: './product-alerts.component.css'
 })
 export class ProductAlertsComponent {
 
   @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
 
 }
